6df4fe3d4e382795692939bd9da9a2b1daca599a
Kumar <kumarks1122@gmail.com>
2018-03-13 17:40:52 +0530

    Social Login Feature Added

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
+import {
+  SocialLoginModule,
+  AuthServiceConfig,
+  GoogleLoginProvider,
+  FacebookLoginProvider,
+} from "angular5-social-login";
 
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
@@ -9,9 +15,24 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent }
 ];
 
+export function getAuthServiceConfigs() {
+  let config = new AuthServiceConfig([
+    {
+      id: FacebookLoginProvider.PROVIDER_ID,
+      provider: new FacebookLoginProvider("173336893473594")
+    },
+    {
+      id: GoogleLoginProvider.PROVIDER_ID,
+      provider: new GoogleLoginProvider("881930825287-20boi6619j24rmpal4aifb1h71g5ctcm.apps.googleusercontent.com")
+    },
+  ]);
+  return config;
+}
+
 @NgModule({
   imports: [
     CommonModule,
+    SocialLoginModule,
     RouterModule.forRoot(routes)
   ],
   declarations: [
@@ -21,6 +42,12 @@ const routes: Routes = [
   exports :[
     RouterModule,
     HeaderComponent
-   ],
+  ],
+  providers: [
+    {
+      provide: AuthServiceConfig,
+      useFactory: getAuthServiceConfigs
+    }
+  ]
 })
 export class CoreModule { }
--- src/app/core/login/login.component.ts
+++ src/app/core/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {
+    AuthService,
+    FacebookLoginProvider,
+    GoogleLoginProvider
+} from 'angular5-social-login';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +13,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private socialAuthService: AuthService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  public socialSignIn(socialPlatform : string) {
+    let socialPlatformProvider;
+    if(socialPlatform == "facebook"){
+      socialPlatformProvider = FacebookLoginProvider.PROVIDER_ID;
+    }else if(socialPlatform == "google"){
+      socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
+    }
+
+    this.socialAuthService.signIn(socialPlatformProvider).then((userData) => {
+      debugger;
+      console.log(socialPlatform+" sign in data : " , userData);
+      this.router.navigate(['/'])
+    });
   }
 
 }
